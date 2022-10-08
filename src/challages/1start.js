a315472f5912d8221b1749367301f86ff10bf484
Dipen parmar <dipensavji@gmail.com>
2022-10-08 10:46:08 +0530

    chore(repo): Created dedicated repo for DSA problems.

---
+++ src/challages/1start.js
@@ -0,0 +1,18 @@
+console.clear()
+console.log(new Date(), '-----------')
+const log = console.log
+
+
+function test(a, b) {
+    return [a]
+}
+
+console.log(
+    test(
+        [10, 150, 5],
+    ),
+
+    test(
+        [17, 28, 30],
+    ),
+)
